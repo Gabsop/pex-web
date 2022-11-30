87088764e8ae4890457e1875fd1149055201f3a5
Gabriel Santana Oliveira de Paula <gabsop@hotmail.com>
2022-11-30 16:08:57 -0300

    ESLint Changes

--- src/components/sidebar/components/Brand.tsx
+++ src/components/sidebar/components/Brand.tsx
@@ -1,20 +1,20 @@
 // Chakra imports
-import { Flex, useColorModeValue } from '@chakra-ui/react';
+import { Flex, useColorModeValue } from "@chakra-ui/react";
 
 // Custom components
-import { HorizonLogo } from 'components/icons/Icons';
-import { HSeparator } from 'components/separator/Separator';
+import { PexLogo } from "components/icons/Icons";
+import { HSeparator } from "components/separator/Separator";
 
 export function SidebarBrand() {
-	//   Chakra color mode
-	let logoColor = useColorModeValue('navy.700', 'white');
+  //   Chakra color mode
+  let logoColor = useColorModeValue("gray.500", "white");
 
-	return (
-		<Flex alignItems='center' flexDirection='column'>
-			<HorizonLogo h='26px' w='175px' my='32px' color={logoColor} />
-			<HSeparator mb='20px' />
-		</Flex>
-	);
+  return (
+    <Flex alignItems="center" flexDirection="column">
+      <PexLogo h="83px" w="153px" mb="20px" mr="100px" color={logoColor} />
+      <HSeparator mb="20px" />
+    </Flex>
+  );
 }
 
 export default SidebarBrand;
