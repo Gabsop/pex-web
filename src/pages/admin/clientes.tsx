b9c72466ebac48fd7e71212d84c233901a7ac6ab
Gabriel Santana Oliveira de Paula <gabsop@hotmail.com>
2022-11-30 16:05:07 -0300

    Added components of sidebar

---
+++ src/pages/admin/clientes.tsx
@@ -0,0 +1,11 @@
+import { Box } from "@chakra-ui/react";
+
+import AdminLayout from "layouts/admin";
+
+export default function Clientes() {
+  return (
+    <AdminLayout>
+      <Box></Box>
+    </AdminLayout>
+  );
+}
---
+++ src/pages/admin/relatorios.tsx
@@ -0,0 +1,11 @@
+import { Box } from "@chakra-ui/react";
+
+import AdminLayout from "layouts/admin";
+
+export default function Relatorios() {
+  return (
+    <AdminLayout>
+      <Box></Box>
+    </AdminLayout>
+  );
+}
